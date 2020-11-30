b753a915d0627a94385393b8b7cd27c3b2307ac3
jake mathews <jacobmathews95@gmail.com>
2020-11-30 16:25:36 -0600

    Created Analyze button which starts analyzing upon press.

--- face_req.py
+++ face_req.py
@@ -4,6 +4,57 @@ import os
 import cv2
 from tkinter import *
 
+def analyzeDatFace():
+    #Finding proper functions and syntax for variables was completed by Josh Sodolak and Caleb Pudden
+    KNOWN_FACES_DIR = "known_faces"
+    UNKNOWN_FACES_DIR = "unknown_faces"
+    TOLERANCE = .8
+    FRAME_THICKNESS = 3
+    FONT_THICKNESS = 2
+    MODEL = "cnn"
+
+    print("loading known faces")
+
+    known_faces = []
+    known_names = []
+
+    #fetching queue of known facial expressions completed by Charlie Pham
+    for name in os.listdir(KNOWN_FACES_DIR):
+        for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
+            image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
+            encoding = face_recognition.face_encodings(image)[0]
+            known_faces.append(encoding)
+            known_names.append(name)
+
+    print("processing unknown faces")
+
+    #Fetching unknown faces, comparison, and display was completed by Jacob Mathews and Maxim Tybar
+    for filename in os.listdir(UNKNOWN_FACES_DIR):
+        print(filename)
+        image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
+        locations = face_recognition.face_locations(image, model=MODEL)
+        encodings = face_recognition.face_encodings(image, locations)
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+        for face_encoding, face_location in zip(encodings, locations):
+            results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
+            match = None
+            if True in results:
+                match = known_names[results.index(True)]
+                print("match found: {match}")
+                top_left = (face_location[3], face_location[0])
+                bottom_right = (face_location[1], face_location[2])
+                color = [0, 255, 0]
+                cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
+
+                top_left = (face_location[3], face_location[2])
+                bottom_right = (face_location[1], face_location[2]+22)
+                cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
+                cv2.putText(image, match, (face_location[3]+10, face_location[2]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200,200,200), FONT_THICKNESS)
+
+        cv2.imshow(filename, image)
+        cv2.waitKey(4000)
+        cv2.destroyWindow(filename)
 
 app = Tk()
 app.title("Emotional Recognition")
@@ -12,68 +63,12 @@ app.configure(background = 'grey')
 
 
 
-uploadButton = Button(app, text = 'Upload photo', bd = '5', command = app.destroy)
-uploadButton.place(x=50, y=555)
+uploadButton = Button(app, text = 'Analyze', bd = '5', command=lambda: analyzeDatFace())
+uploadButton.place(x=245, y=250)
+uploadButton.configure(height = 7, width = 40)
 
-newPictureButton = Button(app, text = 'Take new photo', bd = '5', command = app.destroy)
-newPictureButton.place(x=635, y=555)
 
 app.mainloop()
-
-
-
-#Finding proper functions and syntax for variables was completed by Josh Sodolak and Caleb Pudden
-KNOWN_FACES_DIR = "known_faces"
-UNKNOWN_FACES_DIR = "unknown_faces"
-TOLERANCE = .8
-FRAME_THICKNESS = 3
-FONT_THICKNESS = 2
-MODEL = "cnn"
-
-print("loading known faces")
-
-known_faces = []
-known_names = []
-
-#fetching queue of known facial expressions completed by Charlie Pham
-for name in os.listdir(KNOWN_FACES_DIR):
-    for filename in os.listdir(f"{KNOWN_FACES_DIR}/{name}"):
-        image = face_recognition.load_image_file(f"{KNOWN_FACES_DIR}/{name}/{filename}")
-        encoding = face_recognition.face_encodings(image)[0]
-        known_faces.append(encoding)
-        known_names.append(name)
-
-print("processing unknown faces")
-
-#Fetching unknown faces, comparison, and display was completed by Jacob Mathews and Maxim Tybar
-for filename in os.listdir(UNKNOWN_FACES_DIR):
-    print(filename)
-    image = face_recognition.load_image_file(f"{UNKNOWN_FACES_DIR}/{filename}")
-    locations = face_recognition.face_locations(image, model=MODEL)
-    encodings = face_recognition.face_encodings(image, locations)
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-    for face_encoding, face_location in zip(encodings, locations):
-        results = face_recognition.compare_faces(known_faces, face_encoding, TOLERANCE)
-        match = None
-        if True in results:
-            match = known_names[results.index(True)]
-            print("match found: {match}")
-            top_left = (face_location[3], face_location[0])
-            bottom_right = (face_location[1], face_location[2])
-            color = [0, 255, 0]
-            cv2.rectangle(image, top_left, bottom_right, color, FRAME_THICKNESS)
-
-            top_left = (face_location[3], face_location[2])
-            bottom_right = (face_location[1], face_location[2]+22)
-            cv2.rectangle(image, top_left, bottom_right, color, cv2.FILLED)
-            cv2.putText(image, match, (face_location[3]+10, face_location[2]+15), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (200,200,200), FONT_THICKNESS)
-
-    cv2.imshow(filename, image)
-    cv2.waitKey(4000)
-    cv2.destroyWindow(filename)
-
-
 
 # Youtube tutorial was followed to establish face_recognition import
 #Link: https://www.youtube.com/watch?v=535acCxjHCI
